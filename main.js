641bbd9892e3a6070bf882acf2576e4324d4113d
SaidamirRahimtorayev <saidamirxonrahimtorayev@gmail.com>
2024-06-10 23:13:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let todoInput = document.querySelector(".todo-input");
+let todoBtn = document.querySelector(".todo-btn");
+let todoList = document.querySelector(".todo-list");
+
+
+todoBtn.addEventListener("click", addTodo);
+
+function addTodo(event) {
+    event.preventDefault();
+
+    let todoDiv = document.createElement("div");
+
+    let check = document.createElement("input")
+    check.setAttribute("type", "checkbox")
+    todoDiv.appendChild(check);
+    check.style.width = "18px"
+    check.style.height = "18px"
+
+    let btnTrash = document.createElement("button");
+    btnTrash.style.border = "none"
+    btnTrash.style.width = "18px"
+    btnTrash.style.height = "18px"
+    btnTrash.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    todoDiv.appendChild(btnTrash);
+    todoList.appendChild(todoDiv)
+    todoDiv.style.marginLeft = "40px"
+    todoDiv.style.display = "flex"
+    todoDiv.style.gap = "10px"
+
+    btnTrash.addEventListener("click", function trash(event) {
+        todoDiv.remove();
+    })
+
+}
+
+
+
