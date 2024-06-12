ec382fda08eec2eec2445dfcbe12b1974abca1ce
SaidamirRahimtorayev <saidamirxonrahimtorayev@gmail.com>
2024-06-12 23:31:54 +0500

    first commit

--- main.js
+++ main.js
@@ -15,33 +15,33 @@ function addTodo(event) {
     let check = document.createElement("input")
     check.setAttribute("type", "checkbox")
     todoDiv.appendChild(check);
-    check.style.width = "18px"
-    check.style.height = "18px"
+    check.style.width = "20px"
+    check.style.height = "20px"
     check.style.borderRadius = "30px"
 
     let text = document.createElement("li");
     text.innerHTML = todoInput.value;
     text.style.color = "black"
     text.style.listStyleType = "none"
+    text.style.fontSize = "20px"
     todoDiv.appendChild(text)
 
     let btnTrash = document.createElement("button");
-    btnTrash.style.border = "none"
-    btnTrash.style.width = "18px"
-    btnTrash.style.height = "18px"
-    btnTrash.style.marginLeft = "345px"
     btnTrash.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    btnTrash.style.border = "none"
+    btnTrash.style.marginLeft = "320px"
     todoDiv.appendChild(btnTrash);
     todoList.appendChild(todoDiv)
-    todoDiv.style.marginLeft = "40px"
+    todoDiv.style.width = "455px"
+    todoDiv.style.marginInline = "auto"
     todoDiv.style.display = "flex"
+    todoDiv.style.justifyContent = "center"
+    todoDiv.style.alignItems = "center"
     todoDiv.style.gap = "10px"
 
     btnTrash.addEventListener("click", function trash(event) {
         todoDiv.remove();
     })
 
+
 }
-
-
-
